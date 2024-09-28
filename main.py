1b156e3cd1393f36286749d75681ffaac1acbb9a
Afk-coder-art <shaheenshaikh959@gmail.com>
2024-09-28 18:12:49 +0530

    Encapsulation Implementation activity

---
+++ main.py
@@ -0,0 +1,13 @@
+class Computer:
+    _max_price = 900
+
+    def get_max_price(self):
+        return self._max_price
+    
+    def set_max_price(self, price):
+        self._max_price = price
+
+comp = Computer()
+print(comp.get_max_price())
+comp.set_max_price(3500)
+print(comp.get_max_price())
